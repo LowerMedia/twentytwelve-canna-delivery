905f3d50cf6d29b3cf0d8567f63b5f70b7394179
hawkeye126 <pete.lower@gmail.com>
2014-07-12 11:54:46 -0500

    adding and styling delivery window times

--- header.php
+++ header.php
@@ -55,6 +55,7 @@
 		<hgroup class='header-hgroup'>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h3 class="site-description">Delivery Times Scheduled between 9am - 10pm</h2>
 			<div class='dark-green btn'><?php echo do_shortcode( '[phonenumber]' )?>
 		</div>
 		</hgroup>
